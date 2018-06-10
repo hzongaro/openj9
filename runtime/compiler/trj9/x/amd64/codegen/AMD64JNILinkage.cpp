60f2d5fed7a8993fedcb5cff043530cf8e1473b6
Henry Zongaro <zongaro@ca.ibm.com>
2018-06-10 08:39:41 -0400

    Reverting one more change

--- runtime/compiler/trj9/x/amd64/codegen/AMD64JNILinkage.cpp
+++ runtime/compiler/trj9/x/amd64/codegen/AMD64JNILinkage.cpp
@@ -975,24 +975,13 @@ void TR::AMD64JNILinkage::releaseVMAccess(TR::Node *callNode)
       }
    generateLabelInstruction(JNE4, callNode, longReleaseSnippetLabel, cg());
 
-   TR::SymbolReference *releaseVMAccRef =
-      comp()->getSymRefTab()->findOrCreateReleaseVMAccessSymbolRef(comp()->getMethodSymbol());
-
-   TR::Node *outlineCallNode = TR::Node::createWithSymRef(callNode, TR::icall, 0, releaseVMAccRef);
-
-   TR_OutlinedInstructions *outlinedLongRelease =
-         new (trHeapMemory()) TR_OutlinedInstructions(outlineCallNode, TR::icall,
-                                    NULL, longReleaseSnippetLabel, NULL, cg());
-   cg()->getOutlinedInstructionsList().push_front(outlinedLongRelease);
-
-   cg()->generateDebugCounter(outlinedLongRelease->getFirstInstruction(),
-                 TR::DebugCounter::debugCounterName(comp(),
-                                           "OutlineReleaseVMAccess/%s/(%s)/%d/%d",
-                                           callNode->getOpCode().getName(),
-                                           comp()->signature(),
-                                           callNode->getByteCodeInfo().getCallerIndex(),
-                                           callNode->getByteCodeInfo().getByteCodeIndex()),
-                 1, TR::DebugCounter::Cheap);
+   cg()->addSnippet(
+      new (trHeapMemory()) TR::X86HelperCallSnippet(
+         cg(),
+         callNode,
+         longReleaseRestartLabel,
+         longReleaseSnippetLabel,
+         comp()->getSymRefTab()->findOrCreateReleaseVMAccessSymbolRef(comp()->getMethodSymbol())));
 
    mask = fej9->constReleaseVMAccessMask();
 
