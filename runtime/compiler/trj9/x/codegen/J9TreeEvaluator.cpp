d74ba4e8e6a865c0b1de090d7d824b05f16e573e
Henry Zongaro <zongaro@ca.ibm.com>
2018-06-13 20:05:06 -0400

    Added debug counter for wrtbari VM helper calls.

--- runtime/compiler/trj9/x/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/trj9/x/codegen/J9TreeEvaluator.cpp
@@ -14510,6 +14510,7 @@ void J9::X86::TreeEvaluator::VMwrtbarWithoutStoreEvaluator(
    // PR98283: it is not acceptable to emit a label symbol twice so always generate a new label here
    // we can clean up the API later in a less risky manner
    TR::LabelSymbol *doneLabel = generateLabelSymbol(cg);
+   TR::LabelSymbol *dbgCounterLabel = generateLabelSymbol(cg);
 
    TR::LabelSymbol *cardMarkDoneLabel = NULL;
 
@@ -14904,7 +14905,7 @@ void J9::X86::TreeEvaluator::VMwrtbarWithoutStoreEvaluator(
             }
 
          TR::X86WriteBarrierSnippet *snippet =
-            generateWriteBarrierSnippet(node, TR_WrtbarCardMarkAndOldCheck, owningObjectReg, srcReg, NULL, doneLabel, cg);
+            generateWriteBarrierSnippet(node, TR_WrtbarCardMarkAndOldCheck, owningObjectReg, srcReg, NULL, dbgCounterLabel, cg);
          generateLabelInstruction(JNE4, node, snippet->getSnippetLabel(), snippet->getDependencies(), cg);
 
          // If the destination object is old and not remembered then process the remembered
@@ -14980,11 +14981,22 @@ void J9::X86::TreeEvaluator::VMwrtbarWithoutStoreEvaluator(
          branchOp = JMP4;
          }
 
-      TR::X86WriteBarrierSnippet *snippet = generateWriteBarrierSnippet(node, gcModeForSnippet, owningObjectReg, srcReg, NULL, doneLabel, cg);
+      TR::X86WriteBarrierSnippet *snippet = generateWriteBarrierSnippet(node, gcModeForSnippet, owningObjectReg, srcReg, NULL, dbgCounterLabel, cg);
       generateLabelInstruction(branchOp, node, snippet->getSnippetLabel(), snippet->getDependencies(), cg);
       if (labelAfterBranchToSnippet)
          generateLabelInstruction(LABEL, node, labelAfterBranchToSnippet, cg);
       }
+
+   generateLabelInstruction(JMP4, node, doneLabel, cg);
+   generateLabelInstruction(LABEL, node, dbgCounterLabel, cg);
+   cg->generateDebugCounter(
+             TR::DebugCounter::debugCounterName(comp,
+                                    "wrtbariHelperCalls/%s/(%s)/%d/%d",
+                                    node->getOpCode().getName(),
+                                    comp->signature(),
+                                    node->getByteCodeInfo().getCallerIndex(),
+                                    node->getByteCodeInfo().getByteCodeIndex()),
+             1, TR::DebugCounter::Undetermined);
 
    int32_t numPostConditions = 2 + srm->numAvailableRegisters();
 
