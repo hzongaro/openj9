ba22dd0a8b1f2dff7e924399d53a5ff0b1d54e5e
Henry Zongaro <zongaro@ca.ibm.com>
2021-06-21 13:54:48 -0400

    Use node's owning method to determine where ArrayStoreCHK can be skipped for transformed call to storeFlattenableArrayElement

--- runtime/compiler/optimizer/J9ValuePropagation.cpp
+++ runtime/compiler/optimizer/J9ValuePropagation.cpp
@@ -647,7 +647,7 @@ static TR_YesNoMaybe isValue(TR::VPConstraint *constraint)
 
 static bool owningMethodDoesNotContainStoreChecks(OMR::ValuePropagation *vp, TR::Node *node)
    {
-   TR::ResolvedMethodSymbol *method = node->getSymbolReference()->getOwningMethodSymbol(vp->comp());
+   TR::ResolvedMethodSymbol *method = vp->comp()->getOwningMethodSymbol(node->getOwningMethod());
    if (method && method->skipArrayStoreChecks())
       return true;
    return false;
