8f1fc652e06f500b590467b5422bd1cfd3a5d7df
Henry Zongaro <zongaro@ca.ibm.com>
2021-06-24 17:14:24 -0400

    Define symbol reference for value type array null store check
    Define a new method to find or create a symbol reference for a non-
    helper method that will be used to check whether an array is a
    value-types array, and if so, whether a null value is being stored
    to one of its elements.  If so, a NullPointerException will be
    thrown.
    
    Signed-off-by:  Henry Zongaro <zongaro@ca.ibm.com>

--- runtime/compiler/compile/J9SymbolReferenceTable.cpp
+++ runtime/compiler/compile/J9SymbolReferenceTable.cpp
@@ -2427,6 +2427,19 @@ J9::SymbolReferenceTable::findOrCreateObjectEqualityComparisonSymbolRef()
    return symRef;
    }
 
+TR::SymbolReference *
+J9::SymbolReferenceTable::findOrCreateNonNullableArrayNullStoreCheckSymbolRef()
+   {
+   TR::SymbolReference *symRef = element(nonNullableArrayNullStoreCheckSymbol);
+   if (symRef != NULL)
+      return symRef;
+
+   symRef = self()->findOrCreateCodeGenInlinedHelper(nonNullableArrayNullStoreCheckSymbol);
+   symRef->setCanGCandReturn();
+   symRef->setCanGCandExcept();
+   return symRef;
+   }
+
 TR::ParameterSymbol *
 J9::SymbolReferenceTable::createParameterSymbol(
       TR::ResolvedMethodSymbol *owningMethodSymbol,
