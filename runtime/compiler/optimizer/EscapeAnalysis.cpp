9235eddd5ead772f9c9652e885e1e22458026f47
Henry Zongaro <zongaro@ca.ibm.com>
2022-06-01 16:24:20 -0400

    Add debug counters for local allocations in EA to guage number of opportunities found.

--- runtime/compiler/optimizer/EscapeAnalysis.cpp
+++ runtime/compiler/optimizer/EscapeAnalysis.cpp
@@ -6606,6 +6606,9 @@ void TR_EscapeAnalysis::makeLocalObject(Candidate *candidate)
    TR::Node::recreate(allocationNode, TR::loadaddr);
    allocationNode->setSymbolReference(symRef);
 
+   TR::ByteCodeInfo bcInfo = allocationNode->getByteCodeInfo();
+   TR::DebugCounter::prependDebugCounter(comp(), TR::DebugCounter::debugCounterName(comp(), "escapeAnalysis/contiguous-allocation/%s/(%s)/(%d,%d)", comp()->signature(), bcInfo.getCallerIndex() > -1 ? comp()->getInlinedResolvedMethod(bcInfo.getCallerIndex())->signature(trMemory()): comp()->signature(), bcInfo.getCallerIndex(), bcInfo.getByteCodeIndex(), candidate->_treeTop);
+
    if (candidate->_seenArrayCopy || candidate->_argToCall || candidate->_seenSelfStore || candidate->_seenStoreToLocalObject)
       {
       allocationNode->setCannotTrackLocalUses(true);
@@ -7077,6 +7080,7 @@ void TR_EscapeAnalysis::makeNonContiguousLocalAllocation(Candidate *candidate)
 
   else
       {
+      TR::DebugCounter::prependDebugCounter(comp(), TR::DebugCounter::debugCounterName(comp(), "escapeAnalysis/noncontiguous-allocation/%s/(%s)/(%d,%d)", comp()->signature(), bcInfo.getCallerIndex() > -1 ? comp()->getInlinedResolvedMethod(bcInfo.getCallerIndex())->signature(trMemory()): comp()->signature(), bcInfo.getCallerIndex(), bcInfo.getByteCodeIndex(), candidate->_treeTop);
       // Remove the tree containing the allocation node. All uses of the node
       // should have been removed by now
       //
